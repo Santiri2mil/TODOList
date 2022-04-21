2c9604074f1ae3b29f47cb8dbdf2ff49c6854d3f
Santiri2mil <santiago2mil@gmail.com>
2022-04-21 12:00:42 -0500

    Subida a gIt
    S

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
+import {TextInput, Button, StyleSheet, View, Modal} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const TodoInput = ({ onAddItem }) => {
+
+    const  [item, setItem] = useState('');
+    
+    const onChangeTextHandler = (input) => {
+        setItem(input)
+    }
+
+    const onAdd = () => {
+        onAddItem(item)
+        setItem('')
+        hide()
+    }
+    
+    return (
+        <Modal
+        visivble={visible}
+        >
+            <View style={styles.inputContainer}>
+            <TextInput
+                placeholder='TODO Item'
+                style={styles.TextInput}
+                onChangeText={onChangeTextHandler}
+                value={item}
+            />
+            <Button
+                title='ADD'
+                onPress={onAdd}
+            />
+            </View>
+
+        </Modal>
+        
+    )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    inputContainer: {
+        flexDirection: 'row', //eje principal
+        justifyContent: 'space-between', //eje secundario
+        alignItems: 'center',
+      },
+      TextInput: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        width: '80%'
+      }
+})
+export default TodoInput
